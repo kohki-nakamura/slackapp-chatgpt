c12c1439e68ee4e1d584aee4c244371cc034f0b2
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2024-01-14 14:58:35 +0900

    chat.updateAPI制限を回避する

--- app.py
+++ app.py
@@ -40,16 +40,24 @@ class SlackStreamingCallbackHandler(BaseCallbackHandler):
     def __init__(self, channel, ts):
         self.channel = channel
         self.ts = ts
+        self.interval = CHAT_UPDATE_INTERVAL_SEC
+        # 投稿を更新した累計回数カウンタ
+        self.update_count = 0
 
     def on_llm_new_token(self, token: str, **kwargs) -> None:
         self.message += token
 
         now = time.time()
         if now - self.last_send_time > CHAT_UPDATE_INTERVAL_SEC:
-            self.last_send_time = now
             app.client.chat_update(
-                channel=self.channel, ts=self.ts, text=f"{self.message}..."
+                channel=self.channel, ts=self.ts, text=f"{self.message}\n\nTyping..."
             )
+            self.last_send_time = now
+            self.update_count += 1
+
+            # update_countが現在の更新間隔x10より大きくなるたびに更新間隔を2倍にする
+            if self.update_count / 10 > self.interval:
+                self.interval = self.interval * 2
 
     def on_llm_end(self, response: LLMResult, **kwargs: Any) -> Any:
         app.client.chat_update(channel=self.channel, ts=self.ts, text=self.message)
